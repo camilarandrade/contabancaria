4052e43c96b377928196a73bf078248f432e60f6
Camila Andrade <camila.rda@outlook.com>
2024-06-17 14:59:10 -0300

    inclusão do controller

--- src/conta/repository/ContaRepository.java
+++ src/conta/repository/ContaRepository.java
@@ -3,7 +3,7 @@ package conta.repository;
 import conta.model.Conta;
 
 public interface ContaRepository {
-
+	
 	// CRUD da Conta
 	public void procurarPorNumero(int numero);
 	public void listarTodas();
@@ -15,5 +15,4 @@ public interface ContaRepository {
 	public void sacar(int numero, float valor);
 	public void depositar(int numero, float valor);
 	public void transferir(int numeroOrigem, int numeroDestino, float valor);
-	
 }
